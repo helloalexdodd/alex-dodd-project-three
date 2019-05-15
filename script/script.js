26252f8f0111fd83c21864d8e3e7ed880bdd7c79
helloalexdodd <hello@alexdodd.ca>
2019-05-15 15:56:29 -0400

    pieces can be picked up and placed down only on black squares

--- script/script.js
+++ script/script.js
@@ -7,9 +7,9 @@ $(document).ready(() => {
             boardGrid[i] = [];
             for (let j = 0; j < cols; j++) {
                 if (i % 2 === 0 && j % 2 === 0 || i % 2 !== 0 && j % 2 !== 0) {
-                    boardGrid[i][j] = `<div id="${i}_${j}" class="white"></div>`
+                    boardGrid[i][j] = `<div id="${i}_${j}" class="whiteSquare square" tabIndex="${i + 1}_${j + 1}"></div>`
                 } else {
-                    boardGrid[i][j] = `<div id="${i}_${j}" class="black"></div>`
+                    boardGrid[i][j] = `<div id="${i}_${j}" class="blackSquare square" tabIndex="${i + 1}_${j + 1}"></div>`
                 };
             }
         }
@@ -39,22 +39,48 @@ $(document).ready(() => {
     };
 //listening event for selecting the piece a user wants to move
     $(`#gameBoard`).on(`click`, `.row > div`, function () {
-        if ($(`#gameBoard > div > div`).hasClass(`blackSelected`) || $(`#gameBoard > div > div`).hasClass(`redSelected`)) {
-            if (this.classList.contains(`blackSelected`)) {
-                $(this).removeClass(`blackSelected`)
-            } else if (this.classList.contains(`redSelected`)) {
-                $(this).removeClass(`redSelected`)
-            };
-        } else if ($(`#gameBoard > div > div`).hasClass(`blackSelected` || `redSelected`) === false) {
-            if (this.classList.contains(`blackPiece`)) {
-                $(this).addClass(`blackSelected`)
-            } else if (this.classList.contains(`redPiece`)) {
-                $(this).addClass(`redSelected`)
+        let allSquares = `#gameBoard > div > div`;
+        // let $selectedPiece = $(allSquares).filter('.blackSelected redSelected'); //an array of only selected pieces that I thought I might need one day
+
+        for (let i = 0; i < rows; i++) {
+            boardGrid[i] = [];
+            for (let j = 0; j < cols; j++) {
+                if {
+                };
+            }
+        }    
+        if (this.classList.contains(`blackSquare`)) {
+            if ($(allSquares).hasClass(`blackSelected`) || $(allSquares).hasClass(`redSelected`)) {
+                if (this.classList.contains(`blackSelected`)) {
+                    $(this).removeClass(`blackSelected`)
+                    $(this).addClass(`blackPiece`)
+                } else if (this.classList.contains(`redSelected`)) {
+                    $(this).removeClass(`redSelected`)
+                    $(this).addClass(`redPiece`)
+                } else if (this.classList.contains(`blackSelected`) === false && $(allSquares).hasClass(`blackSelected`)) {
+                    $(this).addClass(`blackPiece`)
+                    $(allSquares).removeClass(`blackSelected`)
+                } else if (this.classList.contains(`redSelected`) === false && $(allSquares).hasClass(`redSelected`)) {
+                    $(this).addClass(`redPiece`)
+                    $(allSquares).removeClass(`redSelected`)
+                };
+            } else if ($(allSquares).hasClass(`blackSelected` || `redSelected`) === false) {
+                if (this.classList.contains(`blackPiece`)) {
+                    $(this).addClass(`blackSelected`)
+                    $(this).removeClass(`blackPiece`)
+                } else if (this.classList.contains(`redPiece`)) {
+                    $(this).addClass(`redSelected`)
+                    $(this).removeClass(`redPiece`)
+                };
             }
         };
-    
-    //write a click event
-
+//create a css class of NO and use a setTimeout function to flash red when user clicks a square they can't place a piece on
+        //if the div is empty
+            //if the div is in a higher or lower i index (depending on the player) than the selected piece (moving backwards), that's a NO
+            //else if the div is in a column more than 2 columns on either side from the piece (j index), that's a NO
+            //else if the div is in a column directly adjacent to itself, add the class of $selected
+            //else if the div is two columns on either side from the piece (j index), check if there's an opponent's piece in the column and row in between the starting and ending point 
+                // write code about what to do if there's an opponent's piece there
+        //else that's a NO
     });
-
 });
