5cbb412b1b226a81ed79ebd545b76d776b0ffe8b
helloalexdodd <hello@alexdodd.ca>
2019-05-16 10:54:10 -0400

    player position is dynamic, players can no longer move backwards now

--- script/script.js
+++ script/script.js
@@ -2,13 +2,22 @@ $(document).ready(() => {
 // an empty array to create our boardGrid inside of
     const boardGrid = [];
     const playerPosition = [];
-// function for setting up the board by looping through nested arrays and creating divs
+    const boardx = [];
+    const boardy = [];
+    let xy;
+    let storedxy;
+// function for setting up the board by looping through nested arrays and creating divs. also fills empty global divs for access to x and y axis
     function setup(rows, cols) {
         for (let i = 0; i < rows; i++) {
             boardGrid[i] = [];
             playerPosition[i] = [];
+            boardx[i] = i;
+            boardy[i] = [];            
             for (let j = 0; j < cols; j++) {
                 playerPosition[i][j] = 0;
+                boardy[j] = j;
+                // boardjAxis[i] = j;
+                // boardjAxis[i][j] = i
                 if (i % 2 === 0 && j % 2 === 0 || i % 2 !== 0 && j % 2 !== 0) {
                     boardGrid[i][j] = `<div id="${i}_${j}" class="whiteSquare square" tabIndex="${i + 1}_${j + 1}"></div>`
                 } else {
@@ -16,40 +25,46 @@ $(document).ready(() => {
                 };
             }
         }
-    };
-// calling the function and passing it the columns and rows
-    setup(8, 8);
-
 // looping through the boardGrid array and appending the divs to the main gameboard
-    boardGrid.forEach((row) => {
-        $(`#gameBoard`).append(`<div class="row">${row.join('')}</div>`)
-    });
-// looping through the boardGrid array and placing the game pieces in their starting positions
-    for (let i = 0; i < boardGrid.length; i++) {
-        for (let j = 0; j < boardGrid.length; j++) {
-            if ((i % 2 === 0 && j % 2 !== 0 || i % 2 !== 0 && j % 2 === 0) && i <= 2) {
-                let cellID = `#${i}_${j}`;
-                $(`#${i}_${j}`).addClass(`blackPiece gamePiece`);
-                playerPosition[i][j] = 1;
-            } else if ((i % 2 === 0 && j % 2 !== 0 || i % 2 !== 0 && j % 2 === 0) && i >= 5) {
-                let cellID = `#${i}_${j}`;
-                $(cellID).addClass(`redPiece gamePiece`);
-                playerPosition[i][j] = 2;
-            }
-        }
+        boardGrid.forEach((row) => {
+            $(`#gameBoard`).append(`<div class="row">${row.join('')}</div>`)
+        });
     };
+
+// function for looping through the boardGrid array and placing the game pieces in their starting positions
+    function setupPieces() {    
+        for (let i = 0; i < boardGrid.length; i++) {
+            for (let j = 0; j < boardGrid.length; j++) {
+                if ((i % 2 === 0 && j % 2 !== 0 || i % 2 !== 0 && j % 2 === 0) && i <= 2) {
+                    let cellID = `#${i}_${j}`;
+                    $(`#${i}_${j}`).addClass(`blackPiece gamePiece`);
+                    playerPosition[i][j] = 1;
+                } else if ((i % 2 === 0 && j % 2 !== 0 || i % 2 !== 0 && j % 2 === 0) && i >= 5) {
+                    let cellID = `#${i}_${j}`;
+                    $(cellID).addClass(`redPiece gamePiece`);
+                    playerPosition[i][j] = 2;
+                }
+            }
+        };
+    };
+
+// calling the setup functions and passing it the columns and rows
+    setup(8, 8);
+    setupPieces();
+
 // listening event for selecting the piece a user wants to move
     $(`#gameBoard`).on(`click`, `.row > div`, function () {
         let allSquares = `#gameBoard > div > div`;
-        const idString = $(this).attr(`id`);
-        let idArray = idString.split("_", 2) ;
-        let idArrayOne = idArray[0];
-        let idArrayTwo = idArray[1];
+// this stuff lets me access the i and j axis of my global arrays by using the id's of each div and passing it           
+    const idString = $(this).attr(`id`);
+    const idArray = idString.split("_", 2) ;
+    const i = idArray[0];
+    const j = idArray[1];
+//variable to hold clicked playerPosition
+        const cellValue = (playerPosition[i][j])
+//variables to hold clicked board positions
         
-        // console.log(idArray)
         //if this click is a black square (because pieces can only move on black squares)
-        // if (idArray[0] % 2 === 0 && idArray[1] % 2 !== 0 || idArray[0] % 2 !== 0 && idArray[1] % 2 === 0 ) {
-        // is this a cleaner way to write it? haha.
         if (this.classList.contains(`blackSquare`)) {
             // if any piece is already selected
             if ($(allSquares).hasClass(`blackSelected`) || $(allSquares).hasClass(`redSelected`)) {
@@ -58,43 +73,66 @@ $(document).ready(() => {
                     // unselect the piece
                     $(this).removeClass(`blackSelected`)
                     // style the piece back to default
-                    $(this).addClass(`blackPiece gamePiece`)
+                    $(this).addClass(`blackPiece`)
+                    // change the playerPosition
+                    playerPosition[i][j] = 1
                 //if this click is already a selected red piece
                 } else if (this.classList.contains(`redSelected`)) {
                     //unselect the piece
                     $(this).removeClass(`redSelected`)
                     //style the piece back to default
-                    $(this).addClass(`redPiece gamePiece`)
-                //if this isn't already where any piece already sits and a black piece somewhere is already selected
-                } else if (this.classList.contains(`gamePiece`) === false && $(allSquares).hasClass(`blackSelected`)) {
-                    //place the piece here
-                    $(this).addClass(`blackPiece gamePiece`)
-                    // remove the piece from its original square
-                    $(allSquares).removeClass(`blackSelected`)
-                // if this isn't already where any piece already sits and a red piece somewhere is already selected
-                } else if (this.classList.contains(`gamePiece`) === false && $(allSquares).hasClass(`redSelected`)) {
-                    //place the piece here
-                    $(this).addClass(`redPiece gamePiece`)
-                    // remove the piece from its original square
-                    $(allSquares).removeClass(`redSelected`)
+                    $(this).addClass(`redPiece`)
+                    // change the playerPosition
+                    playerPosition[i][j] = 2;
+                //if this isn't already where any piece already sits and the piece that's selected is black
+                } else if (/*cellValue === 0 &&*/ $(allSquares).hasClass(`blackSelected`)) {
+                    // store the new x and y axis
+                    xy = [boardx[i], boardy[j]];
+                    // if the piece is being moved forward on the board
+                    if (xy[0] > storedxy[0]) {
+                        //place the piece here
+                        $(this).addClass(`blackPiece`)
+                        // remove the piece from its original square
+                        $(allSquares).removeClass(`blackSelected`)
+                        // change the playerPosition
+                        playerPosition[i][j] = 1;
+                    };
+                    // if this isn't already where any piece already sits and the piece that is selected is red
+                } else if (cellValue === 0 && $(allSquares).hasClass(`redSelected`)) {
+                    // store the new x and y axis
+                    xy = [boardx[i], boardy[j]];
+                    // if the piece is being moved forward on the board
+                    if (xy[0] < storedxy[0]) {
+                        //place the piece here
+                        $(this).addClass(`redPiece`)
+                        // remove the piece from its original square
+                        $(allSquares).removeClass(`redSelected`)
+                        // change the playerPosition
+                        playerPosition[i][j] = 2;
+                    };
                 };
             // if no pieces are already selected
             } else if ($(allSquares).hasClass(`blackSelected` || `redSelected`) === false) {
+                // change the playerPosition
+                playerPosition[i][j] = 0;
+                // store the x and y axis
+                storedxy = [boardx[i], boardy[j]]
+                // storedy = (boardy[j])
                 // if it's a black piece
-                if (this.classList.contains(`blackPiece`)) {
+                if (cellValue === 1) {
                     // select the piece    
                     $(this).addClass(`blackSelected`)
                     // remove styling
-                    $(this).removeClass(`blackPiece gamePiece`)
-                    // change the playerPosition
-                    let cellValue = playerPosition[idArrayOne][idArrayTwo];
-                    console.log(cellValue);
-                    // if it's a red piece
-                } else if (this.classList.contains(`redPiece`)) {
+                    $(this).removeClass(`blackPiece`)
+                    console.log(storedxy[0], storedxy[1])
+                // if it's a red piece
+                } else if (cellValue === 2) {
                     // select the piece
                     $(this).addClass(`redSelected`)
                     // remove styling
-                    $(this).removeClass(`redPiece gamePiece`)
+                    $(this).removeClass(`redPiece`)
+                    // change the playerPosition
+                    console.log(storedxy[0], storedxy[1])
                 };
             };
         };
